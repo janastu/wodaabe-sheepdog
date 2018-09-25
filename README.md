33a422314cf6051d6fa0c5fc8bacee79e0d851fe
Sanketh Kumar <sankethkumar@gmail.com>
2018-09-25 21:40:51 +0530

    Separating ivr and pygsm

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# pyGSM
-Python library for SIMCOM Sim800
+# wodaabe-sheepdog
+IVR system for conducting survey of wodaabe pastoralists
 
 Requirements
 01. Linux OS
 02. Python2
 03. pySerial
+04. pyGSM
---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='wodaabe_bcp',
+      version='1.0',
+      description='IVR to implement Bio-Cultural Protocol for Wodaabe Pastoralists ',
+      author='Sanketh Kumar',
+      author_email='skbakwas@gmail.com',
+      url='https://www.github.com/sankethkumar/pyGSM/',
+      packages=setup.find_packages(),
+	  install_requires=['pyserial'],
+     )
